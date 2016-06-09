b40453c76e4a29362aab4032e7e30c31df5b715f
David Fernandez <dfernandez@afirmatica.com>
2016-06-09 12:08:10 +0200

    Primer ejercicio limpio

---
+++ AnadiendoFuncionalidades/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AnadiendoFuncionalidades.Startup))]
+namespace AnadiendoFuncionalidades
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
