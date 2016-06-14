88206886cac9694f188deb5752072fdebedb9d18
David Fernandez <dfernandez@afirmatica.com>
2016-06-14 12:57:53 +0200

    Añadido practica modelo

---
+++ AnadiendoFuncionalidades/Controllers/FriendsController.cs
@@ -0,0 +1,23 @@
+﻿using AnadiendoFuncionalidades.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnadiendoFuncionalidades.Controllers
+{
+    public class FriendsController : Controller
+    {
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Save(Friend friend)
+        {
+            // TODO: save friend's data
+            //Mensage de error: ModelState.Values.First().Errors.First().ErrorMessage
+            return Content("Entity is valid: " + ModelState.IsValid);
+        }
+    }
+}
---
+++ AnadiendoFuncionalidades/Models/Friend.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace AnadiendoFuncionalidades.Models
+{
+    
+    public class Friend : IValidatableObject
+    {
+        [Required, StringLength(40)]
+        public string Name { get; set; }
+
+        [Range(1, 120)]
+        public int Age { get; set; }
+
+        [EmailAddress, StringLength(50)]
+        public string email { get; set; }
+
+        [Phone]
+        public string telefono { get; set;  }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if(email==null && telefono==null)
+                yield return new ValidationResult(
+                    "Introduce el email o telefono",        // Error message
+                    new[] { "email", "telefono" });            // Invalid property
+        }
+    }
+}
